6aab54c2d08f6d16b881d9d5f5e79daaba3df012
Izak Burger <isburger@gmail.com>
2012-10-17 11:37:21 +0200

    add handler for notifying user about approval.

--- slc/accountrequest/eventhandlers.py
+++ slc/accountrequest/eventhandlers.py
@@ -2,3 +2,7 @@ def createAccount(obj, event):
     # TODO create account here
     # Look up adapters, call them to do actual registration
     pass
+
+def notifyAccountCreated(obj, event):
+    # Send an email to let the user know his account was approved
+    pass
